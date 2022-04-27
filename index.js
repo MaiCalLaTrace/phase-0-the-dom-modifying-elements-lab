d44bcb99b7f4a83641917a9782547908929165c2
maicallatrace <maicallatrace@gmail.com>
2022-04-26 22:45:15 -0600

    REPO

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const body = document.getElementsByTagName("body")[0];
+body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+body.append(newHeader)
